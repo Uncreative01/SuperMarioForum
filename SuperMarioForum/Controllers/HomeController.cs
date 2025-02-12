667e10ed417fee3527b5a41b96a1cc5d22880273
Uncreative01 <jacob.poirier7@gmail.com>
2025-02-12 15:51:51 -0400

    Added New Images

--- SuperMarioForum/Controllers/HomeController.cs
+++ SuperMarioForum/Controllers/HomeController.cs
@@ -1,32 +1,45 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperMarioForum.Data;
 using SuperMarioForum.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SuperMarioForum.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly SuperMarioForumContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SuperMarioForumContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Home/Index
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var discussions = await _context.Discussion
+                .Include(d => d.Comments) // Include comments to count them
+                .OrderByDescending(d => d.CreateDate)
+                .ToListAsync();
+
+            return View(discussions);
         }
 
-        public IActionResult Privacy()
+        // GET: Home/GetDiscussion/{id}
+        public async Task<IActionResult> GetDiscussion(int id)
         {
-            return View();
-        }
+            var discussion = await _context.Discussion
+                .Include(d => d.Comments)
+                .FirstOrDefaultAsync(d => d.DiscussionId == id);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (discussion == null)
+            {
+                return NotFound();
+            }
+
+            return View(discussion);
         }
     }
 }
